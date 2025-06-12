8ea862b15d82b263cb2db3e4e88f59f3dd3883d2
Nooraalaleeli <nooramusabeh1@gmail.com>
2025-06-12 23:56:42 +0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -27,10 +27,6 @@ terraform {
 provider "oci" {
   tenancy_ocid = var.tenancy_ocid
   region       = var.region
-
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
 }
 
 provider "oci" {
@@ -38,17 +34,9 @@ provider "oci" {
   tenancy_ocid = var.tenancy_ocid
   region       = lookup(data.oci_identity_regions.home_region.regions[0], "name")
 
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-}
-
 provider "oci" {
   alias        = "current_region"
   tenancy_ocid = var.tenancy_ocid
   region       = var.region
 
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
 }
